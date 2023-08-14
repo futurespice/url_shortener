6a5c9e78a0b6010b7b288dcdb96b367b266f8f6d
Azat <azatss442@gmail.com>
2023-08-14 10:31:18 +0600

    creating forms of urlsortener

--- urlshortener/views.py
+++ urlshortener/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 # We will use it later
+
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home_view(request):
+    return HttpResponse("Hello world")
